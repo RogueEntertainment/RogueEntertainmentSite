256b15c7d35de820ce2568e0cdbe5717d76aaabd
Elliot A <mostdigitsofpi@gmail.com>
2016-09-23 00:32:21 +0000

    remove html from php branch

--- index.php
+++ index.php
@@ -4,3 +4,24 @@ if(!(isset($_COOKIE["visited_before"]))){
 	header("location:video.php?backpage=index.php");
 }
 ?>
+<!DOCTYPE html>
+<html>
+        <head>
+                <title>Rogue Entertainment</title>
+        </head>
+        <body>
+                <center>
+                        <h1>Rogue Entertainment</h1>
+                </center>
+                <p>
+                        Our projects:
+                        <ul>
+                                <li>
+                                        <a href="projects/trapped.html">
+                                                Trapped
+                                        </a>
+                                </li>
+                        </ul>
+                </p>
+        </body>
+</html>
